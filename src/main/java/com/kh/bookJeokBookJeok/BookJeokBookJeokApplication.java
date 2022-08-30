15eeac14d33553090091f6d9c4a7a2887c71e322
broden-horizon <rlghd1698@naver.com>
2022-08-30 17:50:21 +0900

    feat. make schema for h2

---
+++ src/main/java/com/kh/bookJeokBookJeok/BookJeokBookJeokApplication.java
@@ -0,0 +1,13 @@
+package com.kh.bookJeokBookJeok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookJeokBookJeokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookJeokBookJeokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kh/bookJeokBookJeok/BookJeokBookJeokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.bookJeokBookJeok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookJeokBookJeokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
