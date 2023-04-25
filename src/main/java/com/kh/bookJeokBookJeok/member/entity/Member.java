b8ab6bd326140eb7427e905ec1f4f3ccfbf8e6ea
broden-horizon <rlghd1698@naver.com>
2023-04-25 22:42:30 +0900

    ♻️refactor: Builder 추가, Member에서 Review 탐색할 수 있도록 추, Review에 Member 외래키로 추가

--- src/main/java/com/kh/bookJeokBookJeok/member/entity/Member.java
+++ src/main/java/com/kh/bookJeokBookJeok/member/entity/Member.java
@@ -1,11 +1,24 @@
 package com.kh.bookJeokBookJeok.member.entity;
 
 import com.kh.bookJeokBookJeok.audit.BaseEntity;
+import com.kh.bookJeokBookJeok.review.entity.Review;
 import com.kh.bookJeokBookJeok.wish.entity.Wish;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,39 +29,50 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 public class Member extends BaseEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long memberId;
-    @Column(nullable = false, updatable = false, unique = true)
-    private String email;
-    @Column(nullable = false)
-    private String password;
-    @Column(nullable = false, unique = true)
-    private String nickname;
-    private int point = 0;
-    @Column(name = "LEVELS")
-    private int level = 1;
-    @Enumerated(EnumType.STRING)
-    private MemberStatus memberStatus = MemberStatus.MEMBER_ACTIVE;
-    @ElementCollection(fetch = FetchType.EAGER)
-    private List<String> roles = new ArrayList<>();
-    @OneToMany(mappedBy = "member", fetch = FetchType.LAZY)
-    private List<Wish> wishes = new ArrayList<>();
-    public Member(Long memberId, String email, String password, List<String> roles) {
-        this.memberId = memberId;
-        this.email = email;
-        this.password = password;
-        this.roles = roles;
-    }
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long memberId;
+  @Column(nullable = false, updatable = false, unique = true)
+  private String email;
+  @Column(nullable = false)
+  private String password;
+  @Column(nullable = false, unique = true)
+  private String nickname;
+  private int point = 0;
+  @Column(name = "LEVELS")
+  private int level = 1;
+  @Enumerated(EnumType.STRING)
+  private MemberStatus memberStatus = MemberStatus.MEMBER_ACTIVE;
+  @ElementCollection(fetch = FetchType.EAGER)
+  private List<String> roles = new ArrayList<>();
+  @OneToMany(mappedBy = "member")
+  private List<Wish> wishes = new ArrayList<>();
+  @OneToMany(mappedBy = "member")
+  private List<Review> review = new ArrayList<>();
 
-    @Getter
-    @AllArgsConstructor
-    public enum MemberStatus {
-        MEMBER_ACTIVE(1, "정상"),
-        MEMBER_INACTIVE(2, "휴면"),
-        MEMBER_WITHDRAW(3, "탈퇴");
+  @Builder
+  public Member(String email, String password, String nickname, List<String> roles) {
+    this.email = email;
+    this.password = password;
+    this.nickname = nickname;
+    this.roles = roles;
+  }
 
-        private int code;
-        private String description;
-    }
+  public Member(Long memberId, String email, String password, List<String> roles) {
+    this.memberId = memberId;
+    this.email = email;
+    this.password = password;
+    this.roles = roles;
+  }
+
+  @Getter
+  @AllArgsConstructor
+  public enum MemberStatus {
+    MEMBER_ACTIVE(1, "정상"),
+    MEMBER_INACTIVE(2, "휴면"),
+    MEMBER_WITHDRAW(3, "탈퇴");
+
+    private int code;
+    private String description;
+  }
 }
--- src/main/java/com/kh/bookJeokBookJeok/review/entity/Review.java
+++ src/main/java/com/kh/bookJeokBookJeok/review/entity/Review.java
@@ -1,26 +1,49 @@
 package com.kh.bookJeokBookJeok.review.entity;
 
 import com.kh.bookJeokBookJeok.audit.BaseEntity;
+import com.kh.bookJeokBookJeok.member.entity.Member;
 import com.kh.bookJeokBookJeok.wish.entity.Wish;
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 
 @Entity
 @Getter
-@AllArgsConstructor
-@Setter
 @NoArgsConstructor
 public class Review extends BaseEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long reviewId;
-    private String title;
-    private String writing;
-    @OneToOne
-    @JoinColumn(name = "WISH_ID")
-    private Wish wish;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long reviewId;
+  private String title;
+  private String writing;
+  private String isbn;
+  private String bookTitle;
+  private String author;
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "MEMBER_ID")
+  private Member member;
+  @Setter
+  @OneToOne(mappedBy = "review", cascade = CascadeType.PERSIST)
+  private Wish wish;
+
+  @Builder
+  public Review(String title, String writing, String isbn, String bookTitle, String author, Member member) {
+    this.title = title;
+    this.writing = writing;
+    this.isbn = isbn;
+    this.bookTitle = bookTitle;
+    this.author = author;
+    this.member = member;
+  }
 }
