e276ae1c76421119d83bb7ed65f7e49e6e9c8a6b
broden-horizon <rlghd1698@naver.com>
2023-05-01 14:49:26 +0900

    ♻️refactor: Review Dto 및 Mapper

--- src/main/java/com/kh/bookJeokBookJeok/review/dto/ReviewDto.java
+++ src/main/java/com/kh/bookJeokBookJeok/review/dto/ReviewDto.java
@@ -1,24 +1,31 @@
 package com.kh.bookJeokBookJeok.review.dto;
 
+import com.kh.bookJeokBookJeok.book.dto.BookDto;
 import lombok.*;
 
 import javax.validation.constraints.NotBlank;
 
 public class ReviewDto {
     @Getter
+    @Builder
     static public class Post {
-        @NotBlank
-        private Long wishlistId;
         @NotBlank
         private String title;
         @NotBlank
         private String writing;
+        @NotBlank
+        private String bookTitle;
+        @NotBlank
+        private String isbn;
+        @NotBlank
+        private String author;
     }
     @Getter
-    @Setter
+    @Builder
     static public class Response {
         private Long reviewId;
         private String title;
         private String writing;
+        private BookDto book;
     }
 }
