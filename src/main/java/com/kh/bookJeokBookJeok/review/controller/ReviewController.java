557a282a293e2bf2b2ac421752ece3f1e9ba3fb0
broden-horizon <rlghd1698@naver.com>
2023-05-01 14:48:50 +0900

    ♻️refactor: Review Controller의 create 기능

--- src/main/java/com/kh/bookJeokBookJeok/review/controller/ReviewController.java
+++ src/main/java/com/kh/bookJeokBookJeok/review/controller/ReviewController.java
@@ -1,6 +1,7 @@
 package com.kh.bookJeokBookJeok.review.controller;
 
 
+import com.kh.bookJeokBookJeok.authentication.MemberDetailsService;
 import com.kh.bookJeokBookJeok.dto.SingleResponseDto;
 import com.kh.bookJeokBookJeok.review.dto.ReviewDto;
 import com.kh.bookJeokBookJeok.review.entity.Review;
@@ -9,6 +10,7 @@ import com.kh.bookJeokBookJeok.review.service.ReviewService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,10 +22,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class ReviewController {
     private ReviewMapper reviewMapper;
     private ReviewService reviewService;
+
+    /**
+     * 리뷰를 저장합니다.
+     *
+     * 리뷰한 책 정보가 db에 없다면 생성합니다.
+     * @param post
+     * @param principal
+     * @return
+     */
     @PostMapping
-    public ResponseEntity postReview(@RequestBody ReviewDto.Post post) {
+    public ResponseEntity postReview(@RequestBody ReviewDto.Post post,
+                                     @AuthenticationPrincipal MemberDetailsService.MemberDetails principal) {
         Review review = reviewMapper.postToReview(post);
-        Review response = reviewService.create(review);
+        review = reviewService.create(review, principal.getMemberId());
+        ReviewDto.Response response = reviewMapper.reviewToResponse(review);
         return new ResponseEntity(new SingleResponseDto<>(response), HttpStatus.CREATED);
     }
 }
