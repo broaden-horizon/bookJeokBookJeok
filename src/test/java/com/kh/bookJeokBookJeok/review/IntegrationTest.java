fe8b89a69884778805f8071750b084ea25b6d117
broden-horizon <rlghd1698@naver.com>
2023-05-02 11:10:49 +0900

    test: review get 기능에 대한 통합 테스트

--- src/test/java/com/kh/bookJeokBookJeok/review/IntegrationTest.java
+++ src/test/java/com/kh/bookJeokBookJeok/review/IntegrationTest.java
@@ -1,12 +1,17 @@
 package com.kh.bookJeokBookJeok.review;
 
 import com.google.gson.Gson;
+import com.kh.bookJeokBookJeok.book.entity.Book;
+import com.kh.bookJeokBookJeok.exception.ExceptionCode;
 import com.kh.bookJeokBookJeok.member.entity.Member;
 import com.kh.bookJeokBookJeok.member.repository.MemberRepository;
 import com.kh.bookJeokBookJeok.review.dto.ReviewDto;
+import com.kh.bookJeokBookJeok.review.entity.Review;
+import com.kh.bookJeokBookJeok.review.repository.ReviewRepository;
 import com.kh.bookJeokBookJeok.stub.MockDto;
 import com.kh.bookJeokBookJeok.stub.MockEntity;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +24,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -28,18 +34,20 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @Transactional
 public class IntegrationTest {
+  private final String email = "user@naver.com";
   @Autowired
   private MemberRepository memberRepository;
+  @Autowired
+  private ReviewRepository reviewRepository;
   @Autowired
   private MockMvc mockMvc;
   @Autowired
   private Gson gson;
   private Member member;
-  private final String email = "user@naver.com";
 
   @BeforeAll
   void init() {
-    member =  memberRepository.save(MockEntity.getMember(email));
+    member = memberRepository.save(MockEntity.getMember(email, "paul"));
   }
 
   @Test
@@ -52,7 +60,7 @@ public class IntegrationTest {
 
     // when
     ResultActions resultActions = mockMvc.perform(
-        post("/review")
+        post("/reviews")
             .content(content)
             .accept(MediaType.APPLICATION_JSON)
             .contentType(MediaType.APPLICATION_JSON)
@@ -63,5 +71,73 @@ public class IntegrationTest {
         .andExpect(status().isCreated())
         .andExpect(jsonPath("$.data.title").value(post.getTitle()))
         .andExpect(jsonPath("$.data.book.isbn").value(isbn));
+  }
+
+  @Test
+  @WithUserDetails(value = email, setupBefore = TestExecutionEvent.TEST_EXECUTION)
+  void getReviewTest() throws Exception {
+    // given
+    Review review = saveReview(member);
+
+    // when
+    ResultActions resultActions = mockMvc.perform(
+        get("/reviews/" + review.getReviewId())
+            .accept(MediaType.APPLICATION_JSON)
+    );
+
+    // then
+    resultActions
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.data.title").value(review.getTitle()));
+  }
+
+  @Test
+  @DisplayName("getReivew 예외 테스트 - 없는 리뷰인 경우")
+  @WithUserDetails(value = email, setupBefore = TestExecutionEvent.TEST_EXECUTION)
+  void getReviewTest_Exception() throws Exception {
+    // given
+    Review review = saveReview(member);
+    Long wrongReviewId = 333L;
+
+    // when
+    ResultActions resultActions = mockMvc.perform(
+        get("/reviews/" + wrongReviewId)
+            .accept(MediaType.APPLICATION_JSON)
+    );
+
+    // then
+    resultActions
+        .andExpect(status().isNotFound())
+        .andExpect(jsonPath("$.errors[0].reason").value(ExceptionCode.REVIEW_NOT_FOUND.getDescription()));
+  }
+
+  @Test
+  @DisplayName("getReivew 예외 테스트 - 내가 작성한 리뷰가 아닌 경우")
+  @WithUserDetails(value = email, setupBefore = TestExecutionEvent.TEST_EXECUTION)
+  void getReviewTest_Exception2() throws Exception {
+    // given
+    Member wrongMember = saveMember();
+    Review review = saveReview(wrongMember);
+
+    // when
+    ResultActions resultActions = mockMvc.perform(
+        get("/reviews/" + review.getReviewId())
+            .accept(MediaType.APPLICATION_JSON)
+    );
+
+    // then
+    resultActions
+        .andExpect(status().isUnauthorized())
+        .andExpect(jsonPath("$.errors[0].reason").value(ExceptionCode.NO_ACCESS_TO_REVIEW.getDescription()));
+  }
+
+  private Review saveReview(Member member) {
+    Book book = MockEntity.getBook("1234");
+    Review review = reviewRepository.save(MockEntity.getReview(member, book));
+    return review;
+  }
+
+  private Member saveMember() {
+    return memberRepository.save(MockEntity.getMember());
   }
 }
