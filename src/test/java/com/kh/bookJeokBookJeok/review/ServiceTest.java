39a1fd9c8e2e6fad1ed48b7d2e680dc1a3bf9ac9
broden-horizon <rlghd1698@naver.com>
2023-05-02 10:26:01 +0900

    test: 조회 기능 테스트

--- src/test/java/com/kh/bookJeokBookJeok/review/ServiceTest.java
+++ src/test/java/com/kh/bookJeokBookJeok/review/ServiceTest.java
@@ -2,6 +2,7 @@ package com.kh.bookJeokBookJeok.review;
 
 import com.kh.bookJeokBookJeok.book.entity.Book;
 import com.kh.bookJeokBookJeok.book.service.BookService;
+import com.kh.bookJeokBookJeok.exception.BusinessLogicException;
 import com.kh.bookJeokBookJeok.member.entity.Member;
 import com.kh.bookJeokBookJeok.member.service.MemberService;
 import com.kh.bookJeokBookJeok.review.entity.Review;
@@ -9,14 +10,17 @@ import com.kh.bookJeokBookJeok.review.repository.ReviewRepository;
 import com.kh.bookJeokBookJeok.review.service.ReviewService;
 import com.kh.bookJeokBookJeok.stub.MockEntity;
 import com.kh.bookJeokBookJeok.wish.service.WishService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.TestExecutionListeners;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatNoException;
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
@@ -41,8 +45,6 @@ public class ServiceTest {
     Book book = MockEntity.getBook(isbn);
     Member member = MockEntity.getMember();
     Review review = MockEntity.getReview(member, book);
-
-    // when
     when(memberService.findVerifiedMember(memberId)).thenReturn(member);
     when(bookService.createIfAbsent(any())).thenReturn(book);
 
@@ -50,4 +52,42 @@ public class ServiceTest {
     assertThatNoException().isThrownBy(() -> reviewService.create(review, memberId));
   }
 
+  @Test
+  void getReviewTest() {
+    // given
+    Long memberId = 1L;
+    Long reviewId = 2L;
+    Member member = MockEntity.getMember(memberId);
+    Review review = MockEntity.getReview(member, reviewId);
+
+    when(memberService.findVerifiedMember(memberId)).thenReturn(member);
+    when(reviewRepository.findById(reviewId)).thenReturn(Optional.of(review));
+
+    // when
+    Review reviewGotten = reviewService.getReview(reviewId, memberId);
+
+    // then
+    assertThat(reviewGotten.getReviewId()).isEqualTo(reviewId);
+  }
+
+  @Test
+  @DisplayName("getReivew 예외 테스트 - 본인이 리뷰의 작성자가 아닌 경우")
+  void getReviewTest_excepion1() {
+    // given
+    Long memberId = 1L;
+    Long reviewId = 2L;
+    Member member = MockEntity.getMember(memberId);
+    Review review = MockEntity.getReview(member, reviewId);
+
+    Long wrongMemberId = 333L;
+    Member wrongMember = MockEntity.getMember(wrongMemberId);
+
+    when(memberService.findVerifiedMember(wrongMemberId)).thenReturn(wrongMember);
+    when(reviewRepository.findById(reviewId)).thenReturn(Optional.of(review));
+
+    // when & then
+    assertThatThrownBy(() -> {
+      Review reviewGotten = reviewService.getReview(reviewId, wrongMemberId);
+    }).isInstanceOf(BusinessLogicException.class);
+  }
 }
